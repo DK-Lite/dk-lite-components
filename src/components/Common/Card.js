edf8190bc8adce90f5bca8926a04b54a77c93902
김동기 <dk-lite@gimdong-giui-MacBookPro.local>
2020-03-03 00:40:04 +0900

    update card

--- src/components/Common/Card.js
+++ src/components/Common/Card.js
@@ -1,29 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
 import { LineGraph } from '../Chart'
+
+
+const StyledCard = styled.div`
+    border: 0.5px solid #6663;
+    border-radius: 7px;
+    &:hover {
+        box-shadow: 2px 2px 3px 1px gray;
+    }
+    background: #FFFFFF;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    flex-wrap: wrap;
+`
+
 
 function Card(props){
     const { width, height, ...other } = props;
 
     return(
-        <div
+        <StyledCard
             style= {{
-                border: "0.5px solid #6663",
-                borderRadius: "5px",
-                boxShadow: "0px 0px 2px 1px gray",
-                background: '#FFFFFF',
-                display: 'flex',
-                justifyContent: 'flex-start',
-                flexDirection: 'column',
-                flexWrap: 'wrap',
                 width: width,
                 height: height,
                 ...other
             }} 
         >
             <dib> Title </dib>
-            <LineGraph></LineGraph>
-
-        </div>
+            
+        </StyledCard>
     )
     
 
